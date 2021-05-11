183a3fc971846591e82452fa529f527bb3a3ef55
Alfredo Tlahuel <Vfredo602@gmail.com>
2021-05-11 11:01:29 -0500

    seeds file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,1 +1,27 @@
 const sequelize = require('../config/connection');
+const {Company} = require('../models');
+
+const companyData = require('./companyData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    const companies = await Company.bulkCreate(companyData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    
+  
+    // for (const company of companyData) {
+    //   await Project.create({
+    //     ...project,
+    //     user_id: users[Math.floor(Math.random() * users.length)].id,
+    //   });
+    // }
+  
+    process.exit(0);
+  };
+  
+ seedDatabase();
+  
